965baf9b1e18c771d7174329c98747837640ada5
Nathan Chewning <nchewning1@bcst01.parkland.edu>
2020-05-11 12:59:57 -0500

    made welcome page

---
+++ welcome.php
@@ -0,0 +1,12 @@
+
+<html>
+<head>
+<title>CSC155 001DR Survey Thing</title>
+</head>
+<body>
+This is my welcome page for my project.
+
+If you don't have an account, you can create one <a href='createAccount.php'> here </a>. <br/>
+
+</body>
+
