76e7f1d93b966e3ad44c8498fd3db195da0d5dba
Nathan Chewning <nchewning1@bcst01.parkland.edu>
2020-05-13 12:29:30 -0500

    home page

---
+++ home.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>CSC155001DR-project</title>
+<?php
+
+require ('library/functions.php');
+
+//checkAccount("none");
+checkAccount("user");
+//checkAccount("admin");
+
+$conn = getDBConnection();
+
+?>
+</head>
+<body>
+</body>
+</html>
